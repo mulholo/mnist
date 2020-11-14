d37df0ca54da73be9b27218cb4952e80f53408b7
mulholio <james@jmulholland.com>
2020-11-14 20:12:15 +0000

    implement make_bigger

--- identify.py
+++ identify.py
@@ -4,24 +4,27 @@ import torch
 # TODO Platonic ideals
 # Create 'perfect' digits from simplified matrices and compare to those
 
+
 def to_tensor(img):
     """Converts a PIL image into a pytorch tensor"""
     return torchvision.transforms.ToTensor()(img)
 
+
 small_platonic_matrices = [
     # 0
     [
-        [0,0,0,1,0,0,0],
-        [0,0,1,0,1,0,0],
-        [0,1,0,0,0,1,0],
-        [0,1,0,0,0,1,0],
-        [0,1,0,0,0,1,0],
-        [0,0,1,0,1,0,0],
-        [0,0,0,1,0,0,0],
-     ]
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+    ]
     # TODO add more numbers
     # 1...
 ]
+
 
 def mk_platonic_matrix(n):
     """
@@ -31,10 +34,28 @@ def mk_platonic_matrix(n):
     m = small_platonic_matrices[n]
     return make_bigger(m)
 
+
+flatten = lambda t: [item for sublist in t for item in sublist]
+
+
 def make_bigger(matrix):
     """Takes a 7x7 matrix and makes it into a 28x28 version"""
-    # TODO
-    return matrix
+    return flatten(
+        list(
+            map(
+                lambda row: [row, row, row, row],
+                list(
+                    map(
+                        lambda row: flatten(
+                            list(map(lambda cell: [cell, cell, cell, cell], row))
+                        ),
+                        matrix,
+                    )
+                ),
+            )
+        )
+    )
+
 
 def compare(t1, t2):
     """
@@ -42,18 +63,22 @@ def compare(t1, t2):
     """
     # TODO some average summing
 
+
 def predict(img):
     """Predicts which digit the image represents"""
     t = to_tensor(img)
     # TODO loop through possible images and get a compare score for each platonic
     # TODO return the int with the best score
 
+
 # get dataset
-mnist = torchvision.datasets.MNIST('data', download=True)
+mnist = torchvision.datasets.MNIST("data", download=True)
 
 # get an arbitrary digit at index 0
 item = mnist.__getitem__(0)
 img = item[0]
 result = item[1]
 
-print(predict(img))
+torch.set_printoptions(precision=0, linewidth=160)
+
+# print(predict(img))
